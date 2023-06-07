255acb699e9cb423d9ef43de3f0402f739d07be2
masdika ID <masdikaid@gmail.com>
2023-06-07 21:28:11 +0700

    service worker with workbox

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,12 +1,16 @@
+import * as WorkboxWindow from "workbox-window";
+
 const swRegister = async () => {
-    if ('serviceWorker' in navigator) {
-        try {
-            await navigator.serviceWorker.register('./sw.bundle.js');
-        } catch (error) {
-            console.log('ServiceWorker registration failed: ', error);
-        }
-    } else {
+    if (!'serviceWorker' in navigator) {
         console.log('ServiceWorker not supported in this browser');
+    }
+    const wb = new WorkboxWindow.Workbox('./sw.bundle.js');
+
+    try {
+        await wb.register();
+        console.log('ServiceWorker registration successful');
+    } catch (e) {
+        console.log('ServiceWorker registration failed');
     }
 }
 
