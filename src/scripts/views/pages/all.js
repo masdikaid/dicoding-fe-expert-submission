15c8027acef73d0893cd94b69051ceb7db5dca70
masdika ID <masdikaid@gmail.com>
2023-06-11 21:39:52 +0700

    not found handler

--- src/scripts/views/pages/all.js
+++ src/scripts/views/pages/all.js
@@ -9,15 +9,20 @@ const All = {
                 <h2 class="sub-title">All Restaurant</h2>
             </div>
             <div id="all_explore" class="grid"></div>
+            <p id="not_found_message" class="not-found-message">No Favorite Restaurant Found</p>
         </div>
     `
     },
     afterRender: async () => {
         const restaurants = await RestaurantApi.list();
         const restaurantContainer = document.querySelector('#all_explore');
-        restaurants.forEach((restaurant, index) => {
-            restaurantContainer.innerHTML += RestaurantCard.render(restaurant);
-        });
+        const notFoundMessage = document.querySelector('#not_found_message');
+        if (restaurants.length !== 0) {
+            notFoundMessage.innerHTML = "";
+            restaurants.forEach((restaurant, index) => {
+                restaurantContainer.innerHTML += RestaurantCard.render(restaurant);
+            });
+        }
     }
 }
 
--- src/scripts/views/pages/favourites.js
+++ src/scripts/views/pages/favourites.js
@@ -9,15 +9,20 @@ const Favorite = {
                 <h2 class="sub-title">Favorite Restaurant</h2>
             </div>
             <div id="favorite" class="grid"></div>
+            <p id="not_found_message" class="not-found-message">No Favorite Restaurant Found</p>
         </div>
     `
     },
     afterRender: async () => {
         const restaurants = await RestaurantIdb.getAllRestaurants();
         const restaurantContainer = document.querySelector('#favorite');
-        restaurants.forEach((restaurant, index) => {
-            restaurantContainer.innerHTML += RestaurantCard.render(restaurant);
-        });
+        const notFoundMessage = document.querySelector('#not_found_message');
+        if (restaurants.length !== 0) {
+            notFoundMessage.innerHTML = "";
+            restaurants.forEach((restaurant, index) => {
+                restaurantContainer.innerHTML += RestaurantCard.render(restaurant);
+            });
+        }
     }
 }
 
