e88ae3ad1139e4d02ae38e75933863ebb297bcbc
masdika ID <masdikaid@gmail.com>
2023-07-11 21:42:16 +0700

    e2e like & unlike restaurant

--- e2e/Liking_Restaurant.spec.js
+++ e2e/Liking_Restaurant.spec.js
@@ -1,5 +1,45 @@
 Feature('Liking Restaurant');
 
-Scenario('test something', (I) => {
+Scenario('Visit Page', (I) => {
   I.amOnPage('/');
+  I.see('Explore Restaurant');
+});
+
+Scenario('Restaurant list loaded', async (I) => {
+  I.amOnPage('/');
+  I.seeElement('.card');
+  I.see('See Detail', '.button-cta');
+});
+
+Scenario('View detail restaurant', async (I) => {
+  I.amOnPage('/');
+  I.click(locate('.button-cta')
+    .first());
+});
+
+Scenario('Like a restaurant', async (I) => {
+  I.amOnPage('/');
+  I.click(locate('.button-cta')
+    .first());
+
+  I.seeElement('#favourite');
+  I.see('Tambah Favorit', '#favourite');
+
+  I.click('#favourite');
+  I.see('Favorit', '#favourite');
+});
+
+Scenario('Unlike a restaurant', async (I) => {
+  I.amOnPage('/');
+  I.click(locate('.button-cta')
+    .first());
+
+  I.seeElement('#favourite');
+  I.see('Tambah Favorit', '#favourite');
+
+  I.click('#favourite');
+  I.see('Favorit', '#favourite');
+
+  I.click('#favourite');
+  I.see('Tambah Favorit', '#favourite');
 });
